2aadea90caf5fb87b968e64fc6a56959158a048b
KwonYG <andamirocll@gmail.com>
2020-05-07 17:58:17 +0900

    feat: add Responsive component(#2)

---
+++ src/layouts/Responsive.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Responsive: React.FC = ({ children, ...rest }) => {
+  return <ResponsiveContainer {...rest}>{children}</ResponsiveContainer>
+}
+
+const ResponsiveContainer = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`
+export default Responsive
