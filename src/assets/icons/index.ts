2a6cf61dfe6ca11151b1ba8294fc4a31b25fe437
KwonYG <andamirocll@gmail.com>
2020-05-11 01:51:15 +0900

    chore: formatting tsconfig

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,14 @@
+const icons = {
+  algorithm: {
+    id: 1,
+    src: './algorithm.png',
+    title: 'algorithm',
+    alt: 'algorithm',
+  },
+  docker: { id: 2, src: './docker.png', title: 'docker', alt: 'docker' },
+  /* { id: 3, src: './assets/image03.jpg', title: 'foo', alt: 'bar' },
+  { id: 4, src: './assets/image04.jpg', title: 'foo', alt: 'bar' },
+  { id: 5, src: './assets/image05.jpg', title: 'foo', alt: 'bar' }, */
+}
+
+export default icons
